c26d5f9a733e6e47fb1d786a9ba938b071aa55b7
Fredrik Berglund <fredrik.911225@gmail.com>
2017-02-16 10:31:58 +0100

    Add 'om berwaldhallen' command

--- index.js
+++ index.js
@@ -1,70 +1,16 @@
-/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-           ______     ______     ______   __  __     __     ______
-          /\  == \   /\  __ \   /\__  _\ /\ \/ /    /\ \   /\__  _\
-          \ \  __<   \ \ \/\ \  \/_/\ \/ \ \  _"-.  \ \ \  \/_/\ \/
-           \ \_____\  \ \_____\    \ \_\  \ \_\ \_\  \ \_\    \ \_\
-            \/_____/   \/_____/     \/_/   \/_/\/_/   \/_/     \/_/
 
-
-This is a sample Facebook bot built with Botkit.
-
-This bot demonstrates many of the core features of Botkit:
-
-* Connect to Facebook's Messenger APIs
-* Receive messages based on "spoken" patterns
-* Reply to messages
-* Use the conversation system to ask questions
-* Use the built in storage system to store and retrieve information
-  for a user.
-
+/*
 # RUN THE BOT:
-
   Follow the instructions here to set up your Facebook app and page:
-
     -> https://developers.facebook.com/docs/messenger-platform/implementation
-
-  Run your bot from the command line:
-
-    app_secret=<MY APP SECRET> page_token=<MY PAGE TOKEN> verify_token=<MY_VERIFY_TOKEN> node facebook_bot.js [--lt [--ltsubdomain LOCALTUNNEL_SUBDOMAIN]]
-
-  Use the --lt option to make your bot available on the web through localtunnel.me.
-
-# USE THE BOT:
-
-  Find your bot inside Facebook to send it a direct message.
-
-  Say: "Hello"
-
-  The bot will reply "Hello!"
-
-  Say: "who are you?"
-
-  The bot will tell you its name, where it running, and for how long.
-
-  Say: "Call me <nickname>"
-
-  Tell the bot your nickname. Now you are friends.
-
-  Say: "who am I?"
-
-  The bot will tell you your nickname, if it knows one for you.
-
-  Say: "shutdown"
-
-  The bot will ask if you are sure, and then shut itself down.
 
   Make sure to invite your bot into other channels using /invite @<my bot>!
 
 # EXTEND THE BOT:
-
   Botkit has many features for building cool and useful bots!
-
   Read all about it here:
-
     -> http://howdy.ai/botkit
-
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-
 
 if (!process.env.page_token) {
 	console.log('Error: Specify page_token in environment');
@@ -174,7 +120,7 @@ controller.setupWebserver(process.env.PORT || 3000, function(err, webserver) {
 	});
 });
 
-controller.api.thread_settings.greeting('Hi {{user_first_name}}, welcome to this bot.');
+controller.api.thread_settings.greeting('Hej {{user_first_name}}, välkommen till Berwaldboten.');
 controller.api.thread_settings.get_started('Get Started Payload');
 controller.api.thread_settings.menu([
 	{
@@ -219,6 +165,29 @@ controller.hears(['^(Get Started)'], 'message_received', function(bot, message) 
 	});
 });
 
+controller.hears(['^(Hjälp)'], 'message_received', function(bot, message) {
+	bot.reply(message, {
+		text: 'Du kan välja ett av alternativen här under eller skriva t.ex. "artist Tommy Sjöberg", "hej" eller "berwaldhallen".',
+		quick_replies: [
+			{
+				"content_type": "text",
+				"title": "Vem är du?",
+				"payload": "Vem är du?",
+			},
+			{
+				"content_type": "text",
+				"title": "Ge mig artistinfo",
+				"payload": "artistinfo",
+			},
+			{
+				"content_type": "text",
+				"title": "Om Berwaldhallen",
+				"payload": "berwaldhallen",
+			}
+		]
+	});
+});
+
 controller.hears(['quick'], 'message_received', function(bot, message) {
 
 	bot.reply(message, {
@@ -256,6 +225,45 @@ controller.hears(['^hej', '^hallå', '^tja'], 'message_received', function(bot, 
 				bot.reply(message, user.hasOwnProperty('nickname') ? 'Hej, ' + user.nickname + '!😊' : 'Hallå där.');
 			});
 		}
+	});
+});
+
+controller.hears(['^((om )?(berwald(hallen)?))'], 'message_received', function(bot, message) {
+	let aboutText = 'Konserthuset Berwaldhallen, med Sveriges Radios Symfoniorkester och Radiokören, är en del av Sveriges Radio och en av landets viktigaste kulturinstitutioner med räckvidd långt utanför landets gränser. \nBerwaldhallen är hemmascen för de två ensemblerna Sveriges Radios Symfoniorkester och Radiokören, som båda tillhör de yppersta i Europa inom sina respektive fält. Genom turnéer och framträdanden världen över, har de även blivit viktiga ambassadörer för svensk musik och kultur utomlands. Årligen ges cirka 100 konserter -  i Berwaldhallen och på turnéer och varje sommar arrangeras Östersjöfestivalen här. Samtliga konserter sänds i Sveriges Radio, de flesta i P2, samt runtom och utanför Europa via EBU, European Broadcasting Union.';
+
+	bot.reply(message, aboutText);
+
+	bot.startTyping(message, () => {
+		setTimeout(() => {
+			bot.stopTyping(message, () => {
+				bot.reply(message, {
+					attachment: {
+						'type': 'template',
+						'payload': {
+							'template_type': 'generic',
+							'elements': [
+								{
+									'title': 'Läs mer här',
+									'default_action': {
+										'type': 'web_url',
+										'url': 'https://sverigesradio.se/sida/artikel.aspx?programid=3991&artikel=5848176',
+										'webview_height_ratio': 'tall',
+										'fallback_url': 'https://sverigesradio.se/berwaldhallen'
+									},
+									'buttons': [
+										{
+											'type': 'web_url',
+											'url': 'https://sverigesradio.se/sida/artikel.aspx?programid=3991&artikel=5848176',
+											'title': 'Läs mer'
+										}
+									]
+								}
+							]
+						}
+					}
+				});
+			});
+		}, 1000);
 	});
 });
 
